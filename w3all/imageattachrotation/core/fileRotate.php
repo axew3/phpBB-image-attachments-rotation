6095bfc68c64f63a5053a4589e32bc16f01e9110
alessio nanni <alias@axew3.com>
2022-01-25 18:44:17 +0100

    Update fileRotate.php

--- w3all/imageattachrotation/core/fileRotate.php
+++ w3all/imageattachrotation/core/fileRotate.php
@@ -8,9 +8,6 @@
 * @copyright (c) axew3.com
 * @license GNU General Public License, version 2 (GPL-2.0)
 *
-* For full copyright and license information, please see
-* the docs/CREDITS.txt file.
-*
 */
 
 /**
