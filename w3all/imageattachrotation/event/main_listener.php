b76d04bde723f6c68cdd2c66ff428c56654ca9d1
alessio nanni <alias@axew3.com>
2024-09-19 21:47:29 +0200

    Update main_listener.php

--- w3all/imageattachrotation/event/main_listener.php
+++ w3all/imageattachrotation/event/main_listener.php
@@ -3,7 +3,7 @@
  *
  * Attachments Images Rotation. An extension for the phpBB Forum Software package.
  *
- * @copyright (c) 2021, axew3, https://axew3.com
+ * @copyright (c) 2024, axew3, https://axew3.com
  * @license GNU General Public License, version 2 (GPL-2.0)
  *
  */
@@ -20,67 +20,76 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
  */
 class main_listener implements EventSubscriberInterface
 {
-	public static function getSubscribedEvents()
-	{
-		return array(
-			'core.user_setup' => 'load_language_on_setup',
-	                'core.page_head' => 'overall_header_head_append',
-			'core.page_footer' => 'overall_footer_body_after',
-		);
-	}
-	
-	//protected $config;
-        protected $language;
-	//protected $helper;
-	protected $template;
-	protected $request;
-	protected $phpbb_root_path;	
-	protected $php_ext;
+  public static function getSubscribedEvents()
+  {
+    return array(
+      'core.user_setup' => 'load_language_on_setup',
+                  'core.page_head' => 'overall_header_head_append',
+      'core.page_footer' => 'overall_footer_body_after',
+    );
+  }
 
-	//public function __construct(\phpbb\config\config $config, \phpbb\language\language $language, \phpbb\controller\helper $helper, \phpbb\template\template $template, \phpbb\request\request_interface $request, $phpbb_root_path, $php_ext)
-	public function __construct(\phpbb\language\language $language, \phpbb\template\template $template, \phpbb\request\request_interface $request, $phpbb_root_path, $php_ext)
+  //protected $config;
+        protected $language;
+  //protected $helper;
+  protected $template;
+  protected $request;
+  protected $phpbb_root_path;
+  protected $php_ext;
+
+  //public function __construct(\phpbb\config\config $config, \phpbb\language\language $language, \phpbb\controller\helper $helper, \phpbb\template\template $template, \phpbb\request\request_interface $request, $phpbb_root_path, $php_ext)
+  public function __construct(\phpbb\language\language $language, \phpbb\template\template $template, \phpbb\request\request_interface $request, $phpbb_root_path, $php_ext)
 
 {
-		//$this->config = $config;
-		$this->language = $language;
-		//$this->helper   = $helper;
-		$this->template = $template;
-		$this->request = $request;
-		$this->phpbb_root_path = $phpbb_root_path;
-		$this->php_ext  = $php_ext;
+    //$this->config = $config;
+    $this->language = $language;
+    //$this->helper   = $helper;
+    $this->template = $template;
+    $this->request = $request;
+    $this->phpbb_root_path = $phpbb_root_path;
+    $this->php_ext  = $php_ext;
  }
 
-	public function load_language_on_setup($event)
-	{
-		$lang_set_ext = $event['lang_set_ext'];
-		$lang_set_ext[] = array(
-			'ext_name' => 'w3all/imageattachrotation',
-			'lang_set' => 'common',
-		);
-		$event['lang_set_ext'] = $lang_set_ext;
-	}
-	
+  public function load_language_on_setup($event)
+  {
+    $lang_set_ext = $event['lang_set_ext'];
+    $lang_set_ext[] = array(
+      'ext_name' => 'w3all/imageattachrotation',
+      'lang_set' => 'common',
+    );
+    $event['lang_set_ext'] = $lang_set_ext;
+  }
 
-	public function overall_header_head_append()
-	{
-	}	
-	
-	public function overall_footer_body_after()
-	{ 
-		
-		$w3mode = $this->request->variable('mode', '');
+
+  public function overall_header_head_append()
+  {
+  }
+
+  public function overall_footer_body_after()
+  {
+    global $request;
+    $w3mode = $this->request->variable('mode', '');
+    $w3usid = $this->request->variable('sid', '');
+    #$uagent = $this->request->header('User-Agent');
+    #$uip    = $this->request->server('REMOTE_ADDR');
+
+    $uagent = (!empty($this->request->header('User-Agent'))) ? trim($this->request->header('User-Agent')) : 'unkn0wn';
+    $uip = (!empty($this->request->server('REMOTE_ADDR'))) ? trim($this->request->server('REMOTE_ADDR')) : '127.0.0.1';
 
     // for Quick Reply condition on viewtopic, it is used S_QUICK_REPLY on file ext/w3all/imageattachrotation/styles/prosilver/template/event/overall_footer_body_after.html
-		if( $w3mode != 'edit' && $w3mode != 'post' && $w3mode != 'reply' ){ 
-			$w3mode = '';
-		}
+    if( $w3mode != 'edit' && $w3mode != 'post' && $w3mode != 'reply' ){
+      $w3mode = '';
+    }
 
-		$this->template->assign_vars(array( 
-		 'W3IMAGEROTATION_YN_A'	=> $w3mode,
-	 //'W3IMAGEROTATION_COOKIE'	=> $cookie_domain,
+    $this->template->assign_vars(array(
+     'W3IMAGEROTATION_YN_A' => $w3mode,
+     'W3IMAGEROTATION_USID' => $w3usid,
+     'W3IMAGEROTATION_UAGENT' => $uagent,
+     'W3IMAGEROTATION_UIP'  => $uip,
+   //'W3IMAGEROTATION_COOKIE' => $cookie_domain,
     ));
-		
-	 }
-	
-	
+
+   }
+
+
 }
